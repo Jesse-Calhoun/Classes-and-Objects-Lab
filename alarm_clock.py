1d4211381d4e5578e6fb883b0fd046749f535cdb
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2022-12-16 17:52:33 -0500

    In alarm_clock.py file unneccessary code was removed. In product_py file I created some class variables product_name, product_price, and product_category. I adjust the __init__ () by adding the parameters product_name, product_price and product_category. I then tested the code to make sure it was working properly below by assign the class Product('Apple', 3 and 'Fruit') to the object my_favorite. I then used the print() function and dot notation to print the product_price of the my_favorite object.

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,14 +1,9 @@
 class AlarmClock:
-    current_time = ''
-    alarm_set_for = ''
+
     def __init__(self, current_time, alarm_on_or_off, alarm_set_for):
         self.current_time = current_time
         self.alarm_is_on = alarm_on_or_off
         self.alarm_set_for = alarm_set_for
-        # if self.alarm_is_on == True:
-        #         self.alarm_is_on = 'Alarm is on.'
-        # else:
-        #         self.alarm_is_on = 'Alarm is off.'
     def change_current_time(self, new_time):
         self.current_time = new_time
         print(self.current_time)
@@ -21,17 +16,16 @@ class AlarmClock:
     def set_alarm_time(self, alarm_time):
         self.alarm_set_for = alarm_time
         print(self.alarm_set_for)
-        # return self.alarm_is_on
     
-# jesses_alarm = AlarmClock('2:25PM',True, '6:00PM')
-# print(jesses_alarm.current_time)
-# print()
-# jesses_alarm.change_current_time('4:30PM')
-# print()
-# print(jesses_alarm.alarm_is_on)
-# jesses_alarm.toggle_alarm()
-# print()
-# print(jesses_alarm.alarm_is_on)
-# print()
-# print(jesses_alarm.alarm_set_for)
-# jesses_alarm.set_alarm_time('6:30PM')
+jesses_alarm = AlarmClock('2:25PM',True, '6:00PM')
+print(jesses_alarm.current_time)
+print()
+jesses_alarm.change_current_time('4:30PM')
+print()
+print(jesses_alarm.alarm_is_on)
+jesses_alarm.toggle_alarm()
+print()
+print(jesses_alarm.alarm_is_on)
+print()
+print(jesses_alarm.alarm_set_for)
+jesses_alarm.set_alarm_time('6:30PM')
--- product.py
+++ product.py
@@ -1,4 +1,13 @@
 
 class Product:
-    def __init__(self,):
-        pass
+   product_name = ''
+   product_price = 0
+   product_category = ''
+   def __init__(self, product_name, product_price, product_category):
+        self.product_name = product_name
+        self.product_price = product_price
+        self.product_category = product_category
+
+my_favorite = Product('Apple', 3, "Fruit")
+print(my_favorite.product_price)
+my_favorite.print(Product)
