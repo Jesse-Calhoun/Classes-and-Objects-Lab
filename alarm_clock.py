3df92916916df8f0017a3b712aa4494f3493c508
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2022-12-16 16:44:59 -0500

    I created the method set_alarm_time with the parameters: self and alarm_time. I assigned the value of the parameter alarm_time to the instance variable self.alarm_set_for. Then printed the adjusted self.alarm_set_for.

--- alarm_clock.py
+++ alarm_clock.py
@@ -3,29 +3,35 @@ class AlarmClock:
     alarm_set_for = ''
     def __init__(self, current_time, alarm_on_or_off, alarm_set_for):
         self.current_time = current_time
-        self.alarm_on_or_off = alarm_on_or_off
+        self.alarm_is_on = alarm_on_or_off
         self.alarm_set_for = alarm_set_for
-        if self.alarm_on_or_off == True:
-                self.alarm_on_or_off = 'Alarm is on.'
-        else:
-                self.alarm_on_or_off = 'Alarm is off.'
+        # if self.alarm_is_on == True:
+        #         self.alarm_is_on = 'Alarm is on.'
+        # else:
+        #         self.alarm_is_on = 'Alarm is off.'
     def change_current_time(self, new_time):
         self.current_time = new_time
         print(self.current_time)
     def toggle_alarm(self):
-        self.alarm_on_or_off = not self.alarm_on_or_off
-        if self.alarm_on_or_off == True:
-                self.alarm_on_or_off = 'Alarm has been turned on.'
+        self.alarm_is_on = not self.alarm_is_on
+        if self.alarm_is_on:
+                print('Alarm has been turned on.')
         else:
-                self.alarm_on_or_off = 'Alarm has been turned off.'
-        return self.alarm_on_or_off
+                print('Alarm has been turned off.')
+    def set_alarm_time(self, alarm_time):
+        self.alarm_set_for = alarm_time
+        print(self.alarm_set_for)
+        # return self.alarm_is_on
     
 jesses_alarm = AlarmClock('2:25PM',True, '6:00PM')
 print(jesses_alarm.current_time)
 print()
 jesses_alarm.change_current_time('4:30PM')
 print()
-print(jesses_alarm.alarm_on_or_off)
+print(jesses_alarm.alarm_is_on)
 jesses_alarm.toggle_alarm()
 print()
-print(jesses_alarm.alarm_on_or_off)
+print(jesses_alarm.alarm_is_on)
+print()
+print(jesses_alarm.alarm_set_for)
+jesses_alarm.set_alarm_time('6:30PM')
