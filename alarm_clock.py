b768c3bc82954666b0c05c4a5abee9d739ebe52e
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2022-12-16 16:03:39 -0500

    I created the method set_alarm with only the selft parameter. in the scope of set_alarm I assigned self.'alarm_on_or_off' the value of 'not self.alarm_on_or_off', and by doing that toggling the boolean within self.alarm_on_or_off from either True to False or from False to True. Then the newly toggled self.alarm_on_or_off will be run through an if/else statement that would print according to the boolean whether the alarm is turned on or turned off. I then return the variable self.alarm_on_or_off back into the class.

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,4 +1,3 @@
-
 class AlarmClock:
     current_time = ''
     alarm_set_for = ''
@@ -9,17 +8,24 @@ class AlarmClock:
         if self.alarm_on_or_off == True:
                 self.alarm_on_or_off = 'Alarm is on.'
         else:
-                return 'Alarm is off.'
-    
+                self.alarm_on_or_off = 'Alarm is off.'
     def change_current_time(self, new_time):
         self.current_time = new_time
         print(self.current_time)
+    def set_alarm(self,):
+        self.alarm_on_or_off = not self.alarm_on_or_off
+        if self.alarm_on_or_off == True:
+                self.alarm_on_or_off = 'Alarm is turned on.'
+        else:
+                self.alarm_on_or_off = 'Alarm is turned off.'
+        return self.alarm_on_or_off
     
-    #def set_alarm_clock(self, ):
 jesses_alarm = AlarmClock('2:25PM',True, '6:00PM')
-print()
 print(jesses_alarm.current_time)
 print()
-jesses_alarm.change_current_time('3:30PM')
+jesses_alarm.change_current_time('4:30PM')
+print()
+print(jesses_alarm.alarm_on_or_off)
+jesses_alarm.set_alarm()
 print()
 print(jesses_alarm.alarm_on_or_off)
